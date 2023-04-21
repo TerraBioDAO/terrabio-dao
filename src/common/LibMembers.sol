70f06874295f31de06d0a4061922cbe7a24fd2e2
raphael <raphael.pellet@protonmail.com>
2023-04-21 22:21:37 +0200

    👨‍👩‍👧‍👦 members storage

---
+++ src/common/LibMembers.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import {EnumerableSet} from "openzeppelin-contracts/utils/structs/EnumerableSet.sol";
+
+library LibMembers {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    struct Data {
+        EnumerableSet.AddressSet members;
+    }
+
+    /// @dev Storage slot for Data struct
+    bytes32 internal constant STORAGE_SLOT =
+        keccak256("terrabiodao.contracts.storage.Members.v1");
+
+    /// @return data Data struct at `STORAGE_SLOT`
+    function accessData() internal pure returns (Data storage data) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            data.slot := slot
+        }
+    }
+}
