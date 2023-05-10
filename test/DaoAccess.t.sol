2db9fa2f13c1879d65635a89cec2cacd4b9368ba
raphael <raphael.pellet@protonmail.com>
2023-05-10 15:54:28 +0200

    adapt tests with new base

--- test/DaoAccess.t.sol
+++ test/DaoAccess.t.sol
@@ -2,23 +2,18 @@
 
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import { BaseTest } from "test/base/BaseTest.t.sol";
+import { ADMIN_ROLE, MEMBER_ROLE } from "src/dao_access/Roles.sol";
 
-import {ADMIN_ROLE, MEMBER_ROLE} from "src/dao_access/Roles.sol";
-import {DaoAccess} from "src/dao_access/DaoAccess.sol";
-import {RoleGated} from "test/mocks/RoleGated.sol";
+import { DaoAccess } from "src/dao_access/DaoAccess.sol";
+import { RoleGated } from "test/mocks/RoleGated.sol";
 
-contract DaoAccess_test is Test {
-    DaoAccess internal access;
-    address internal ACCESS;
+contract DaoAccess_test is BaseTest {
+    DaoAccess internal access_impl;
+    DaoAccess internal dao;
 
     RoleGated internal roleGated;
     address internal ROLE_GATED;
-
-    address internal constant OWNER = address(501);
-    address internal constant USER = address(1);
-    address internal constant VISITOR = address(2);
-    address internal constant VISITOR2 = address(3);
 
     bytes32 internal constant ROLE_3 = bytes32(uint256(2 ** 3));
     bytes32 internal constant ROLE_4 = bytes32(uint256(2 ** 4));
@@ -26,198 +21,186 @@ contract DaoAccess_test is Test {
     bytes32 internal constant ROLE_6 = bytes32(uint256(2 ** 6));
 
     function setUp() public {
-        access = new DaoAccess(OWNER);
-        ACCESS = address(access);
-        vm.prank(OWNER);
-        access.bootstrap();
+        _newUsersSet(0, 5);
+        (, access_impl) = _deployDAO();
+        dao = DaoAccess(DAO);
 
         roleGated = new RoleGated();
         ROLE_GATED = address(roleGated);
 
-        vm.label(OWNER, "OWNER");
-        vm.label(USER, "USER");
-        vm.label(VISITOR, "VISITOR");
-        vm.label(VISITOR2, "VISITOR2");
+        // set a mock role gated on the DAO
+        _addFunction(RoleGated.gate.selector, ROLE_GATED);
+        _addFunction(RoleGated.adminGate.selector, ROLE_GATED);
+        roleGated = RoleGated(DAO);
     }
 
     function test_constructor_bootstrap(address addr) public {
-        vm.assume(addr != OWNER);
-        assertTrue(access.hasRole(ADMIN_ROLE, OWNER));
-        assertFalse(access.hasRole(ADMIN_ROLE, addr));
+        vm.assume(addr != OWNER && addr != DAO);
+        assertTrue(dao.hasRole(ADMIN_ROLE, OWNER));
+        assertTrue(dao.hasRole(ADMIN_ROLE, DAO));
+        assertFalse(dao.hasRole(ADMIN_ROLE, addr));
     }
 
     function test_grantRole_GrantRole(address addr) public {
         vm.prank(OWNER);
-        access.grantRole(ROLE_3, addr);
+        dao.grantRole(ROLE_3, addr);
 
-        assertTrue(access.hasRole(ROLE_3, addr));
+        assertTrue(dao.hasRole(ROLE_3, addr));
     }
 
     function test_grantRole_CannotGrantRole() public {
         vm.expectRevert(
-            abi.encodeWithSignature(
-                "NotRoleOperator(bytes32,bytes32)",
-                ROLE_3,
-                ADMIN_ROLE
-            )
+            abi.encodeWithSignature("NotRoleOperator(bytes32,bytes32)", ROLE_3, ADMIN_ROLE)
         );
-        access.grantRole(ROLE_3, USER);
+        dao.grantRole(ROLE_3, USERS[0]);
     }
 
     function test_grantRole_GrantSeveralRolesAtOnce() public {
         vm.prank(OWNER);
-        access.grantRole(ROLE_3 | ROLE_4 | ROLE_5, USER);
+        dao.grantRole(ROLE_3 | ROLE_4 | ROLE_5, USERS[0]);
 
-        assertTrue(access.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_4 | ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_3 | ROLE_4, USER));
-        assertTrue(access.hasRole(ROLE_3 | ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_4, USER));
-        assertTrue(access.hasRole(ROLE_3, USER));
+        assertTrue(dao.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_4 | ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_3 | ROLE_4, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_3 | ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_4, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_3, USERS[0]));
     }
 
     function test_grantRole_GrantSeveralRoles() public {
         vm.startPrank(OWNER);
-        access.grantRole(ROLE_5, USER);
-        access.grantRole(ROLE_4, USER);
-        access.grantRole(ROLE_3, USER);
+        dao.grantRole(ROLE_5, USERS[0]);
+        dao.grantRole(ROLE_4, USERS[0]);
+        dao.grantRole(ROLE_3, USERS[0]);
 
-        assertTrue(access.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_4 | ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_3 | ROLE_4, USER));
-        assertTrue(access.hasRole(ROLE_3 | ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_4, USER));
-        assertTrue(access.hasRole(ROLE_3, USER));
+        assertTrue(dao.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_4 | ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_3 | ROLE_4, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_3 | ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_4, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_3, USERS[0]));
     }
 
     function test_grantRole_OperatorGrantRole() public {
         // ROLE_3 can grant ROLE_4
         vm.startPrank(OWNER);
-        access.setAdminRole(ROLE_4, ROLE_3);
-        access.grantRole(ROLE_3, USER);
+        dao.setAdminRole(ROLE_4, ROLE_3);
+        dao.grantRole(ROLE_3, USERS[0]);
         vm.stopPrank();
 
-        assertEq(access.getRoleAdmin(ROLE_4), ROLE_3);
-        assertTrue(access.hasRole(ROLE_3, USER));
+        assertEq(dao.getRoleAdmin(ROLE_4), ROLE_3);
+        assertTrue(dao.hasRole(ROLE_3, USERS[0]));
 
-        vm.prank(USER);
-        access.grantRole(ROLE_4, VISITOR);
-        assertTrue(access.hasRole(ROLE_4, VISITOR));
+        vm.prank(USERS[0]);
+        dao.grantRole(ROLE_4, USERS[1]);
+        assertTrue(dao.hasRole(ROLE_4, USERS[1]));
     }
 
     function test_revokeRole_RevokeRole() public {
-        test_grantRole_GrantRole(USER); // reuse test
+        test_grantRole_GrantRole(USERS[0]); // reuse test
         vm.prank(OWNER);
-        access.revokeRole(ROLE_3, USER);
-        assertFalse(access.hasRole(ROLE_3, USER));
+        dao.revokeRole(ROLE_3, USERS[0]);
+        assertFalse(dao.hasRole(ROLE_3, USERS[0]));
     }
 
     function test_revokeRole_CannotRevokeRole() public {
-        test_grantRole_GrantRole(USER);
+        test_grantRole_GrantRole(USERS[0]);
         vm.expectRevert(
-            abi.encodeWithSignature(
-                "NotRoleOperator(bytes32,bytes32)",
-                ROLE_3,
-                ADMIN_ROLE
-            )
+            abi.encodeWithSignature("NotRoleOperator(bytes32,bytes32)", ROLE_3, ADMIN_ROLE)
         );
-        access.revokeRole(ROLE_3, USER);
+        dao.revokeRole(ROLE_3, USERS[0]);
     }
 
     function test_revokeRole_RevokeSeveralRoleAtOnce() public {
         test_grantRole_GrantSeveralRolesAtOnce();
         vm.prank(OWNER);
-        access.revokeRole(ROLE_3 | ROLE_4 | ROLE_5, USER);
+        dao.revokeRole(ROLE_3 | ROLE_4 | ROLE_5, USERS[0]);
 
-        assertFalse(access.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USER));
-        assertFalse(access.hasRole(ROLE_4 | ROLE_5, USER));
-        assertFalse(access.hasRole(ROLE_3 | ROLE_4, USER));
-        assertFalse(access.hasRole(ROLE_3 | ROLE_5, USER));
-        assertFalse(access.hasRole(ROLE_5, USER));
-        assertFalse(access.hasRole(ROLE_4, USER));
-        assertFalse(access.hasRole(ROLE_3, USER));
+        assertFalse(dao.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_4 | ROLE_5, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_3 | ROLE_4, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_3 | ROLE_5, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_5, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_4, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_3, USERS[0]));
     }
 
     function test_revokeRole_RevokeOneRoleAmongSeveral() public {
         test_grantRole_GrantSeveralRolesAtOnce();
         vm.prank(OWNER);
-        access.revokeRole(ROLE_5, USER);
+        dao.revokeRole(ROLE_5, USERS[0]);
 
-        assertFalse(access.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USER));
-        assertFalse(access.hasRole(ROLE_4 | ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_3 | ROLE_4, USER));
-        assertFalse(access.hasRole(ROLE_3 | ROLE_5, USER));
-        assertFalse(access.hasRole(ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_4, USER));
-        assertTrue(access.hasRole(ROLE_3, USER));
+        assertFalse(dao.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_4 | ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_3 | ROLE_4, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_3 | ROLE_5, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_4, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_3, USERS[0]));
     }
 
     function test_revokeRole_RevokeTwoRoleAmongSeveral() public {
         test_grantRole_GrantSeveralRolesAtOnce();
         vm.prank(OWNER);
-        access.revokeRole(ROLE_3 | ROLE_4, USER);
+        dao.revokeRole(ROLE_3 | ROLE_4, USERS[0]);
 
-        assertFalse(access.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USER));
-        assertFalse(access.hasRole(ROLE_4 | ROLE_5, USER));
-        assertFalse(access.hasRole(ROLE_3 | ROLE_4, USER));
-        assertFalse(access.hasRole(ROLE_3 | ROLE_5, USER));
-        assertTrue(access.hasRole(ROLE_5, USER));
-        assertFalse(access.hasRole(ROLE_4, USER));
-        assertFalse(access.hasRole(ROLE_3, USER));
+        assertFalse(dao.hasRole(ROLE_3 | ROLE_4 | ROLE_5, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_4 | ROLE_5, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_3 | ROLE_4, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_3 | ROLE_5, USERS[0]));
+        assertTrue(dao.hasRole(ROLE_5, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_4, USERS[0]));
+        assertFalse(dao.hasRole(ROLE_3, USERS[0]));
     }
 
     function test_revokeRole_OperatorCanRevoke() public {
         test_grantRole_OperatorGrantRole();
-        vm.prank(USER);
-        access.revokeRole(ROLE_4, VISITOR);
-        assertFalse(access.hasRole(ROLE_4, VISITOR));
+        vm.prank(USERS[0]);
+        dao.revokeRole(ROLE_4, USERS[1]);
+        assertFalse(dao.hasRole(ROLE_4, USERS[1]));
     }
 
     function test_renounceRole_RoleOwnerCanRenounceRole() public {
-        test_grantRole_GrantRole(USER);
-        vm.prank(USER);
-        access.renounceRole(ROLE_3, USER);
-        assertFalse(access.hasRole(ROLE_3, USER));
+        test_grantRole_GrantRole(USERS[0]);
+        vm.prank(USERS[0]);
+        dao.renounceRole(ROLE_3, USERS[0]);
+        assertFalse(dao.hasRole(ROLE_3, USERS[0]));
     }
 
     function test_renounceRole_OnlyOwnerCanRenounceRole() public {
-        test_grantRole_GrantRole(USER);
-        vm.prank(VISITOR);
+        test_grantRole_GrantRole(USERS[0]);
+        vm.prank(USERS[1]);
         vm.expectRevert(abi.encodeWithSignature("NotSelfRenouncement()"));
-        access.renounceRole(ROLE_3, USER);
+        dao.renounceRole(ROLE_3, USERS[0]);
 
         vm.prank(OWNER);
         vm.expectRevert(abi.encodeWithSignature("NotSelfRenouncement()"));
-        access.renounceRole(ROLE_3, USER);
+        dao.renounceRole(ROLE_3, USERS[0]);
     }
 
     function test_setAdminRole_AssignOperatorRole() public {
         vm.prank(OWNER);
-        access.setAdminRole(ROLE_3, ROLE_4);
+        dao.setAdminRole(ROLE_3, ROLE_4);
 
-        assertEq(access.getRoleAdmin(ROLE_3), ROLE_4);
+        assertEq(dao.getRoleAdmin(ROLE_3), ROLE_4);
     }
 
     function test_setAdminRole_NewOperatorOverridePrevious() public {
         test_setAdminRole_AssignOperatorRole();
         vm.prank(OWNER);
-        access.setAdminRole(ROLE_3, ROLE_5);
+        dao.setAdminRole(ROLE_3, ROLE_5);
 
-        assertEq(access.getRoleAdmin(ROLE_3), ROLE_5);
+        assertEq(dao.getRoleAdmin(ROLE_3), ROLE_5);
     }
 
     function test_setAdminRole_OnlyAdminCanAssignOperator() public {
-        vm.prank(USER);
+        vm.prank(USERS[0]);
         vm.expectRevert(
-            abi.encodeWithSignature(
-                "MissingRole(address,bytes32)",
-                USER,
-                ADMIN_ROLE
-            )
+            abi.encodeWithSignature("MissingRole(address,bytes32)", USERS[0], ADMIN_ROLE)
         );
-        access.setAdminRole(ROLE_3, ROLE_5);
+        dao.setAdminRole(ROLE_3, ROLE_5);
     }
 
     /*////////////////////////////////////////////////////////////////////////////////////////////////
@@ -225,11 +208,10 @@ contract DaoAccess_test is Test {
     ////////////////////////////////////////////////////////////////////////////////////////////////*/
 
     function test_onlyRole_OnlyAllowedRoleAccess() public {
-        test_grantRole_GrantRole(USER);
-        assertTrue(access.hasRole(ADMIN_ROLE, OWNER));
-        workaround_UseAccessStorageForRoleGated();
+        test_grantRole_GrantRole(USERS[0]);
+        assertTrue(dao.hasRole(ADMIN_ROLE, OWNER));
 
-        vm.prank(USER);
+        vm.prank(USERS[0]);
         assertTrue(roleGated.gate(ROLE_3));
 
         vm.prank(OWNER);
@@ -237,46 +219,30 @@ contract DaoAccess_test is Test {
     }
 
     function test_onlyRole_OnlyAllowedRolesAccess() public {
-        test_grantRole_GrantRole(USER);
+        test_grantRole_GrantRole(USERS[0]);
         vm.prank(OWNER);
-        access.grantRole(ROLE_5, VISITOR);
+        dao.grantRole(ROLE_5, USERS[1]);
         vm.prank(OWNER);
-        access.grantRole(ROLE_5 | ROLE_3, VISITOR2);
-        workaround_UseAccessStorageForRoleGated();
+        dao.grantRole(ROLE_5 | ROLE_3, USERS[2]);
 
-        vm.prank(USER);
+        vm.prank(USERS[0]);
         assertTrue(roleGated.gate(ROLE_3));
         // assertTrue(roleGated.gate(ROLE_3 | ROLE_5)); not work
 
-        vm.prank(VISITOR);
+        vm.prank(USERS[1]);
         assertTrue(roleGated.gate(ROLE_5));
         // assertTrue(roleGated.gate(ROLE_3 | ROLE_5)); not work
 
         // work only when both role has been granted
-        vm.prank(VISITOR2);
+        vm.prank(USERS[2]);
         assertTrue(roleGated.gate(ROLE_5 | ROLE_3));
     }
 
     function test_onlyRole_BlockUnauthorizedMembers() public {
-        test_grantRole_GrantRole(USER);
-        workaround_UseAccessStorageForRoleGated();
+        test_grantRole_GrantRole(USERS[0]);
 
-        vm.prank(USER);
-        vm.expectRevert(
-            abi.encodeWithSignature(
-                "MissingRole(address,bytes32)",
-                USER,
-                ROLE_4
-            )
-        );
+        vm.prank(USERS[0]);
+        vm.expectRevert(abi.encodeWithSignature("MissingRole(address,bytes32)", USERS[0], ROLE_4));
         roleGated.gate(ROLE_4);
-    }
-
-    /*////////////////////////////////////////////////////////////////////////////////////////////////
-                                                WORKAROUNDS
-    ////////////////////////////////////////////////////////////////////////////////////////////////*/
-    function workaround_UseAccessStorageForRoleGated() internal {
-        vm.etch(ACCESS, ROLE_GATED.code);
-        roleGated = RoleGated(ACCESS);
     }
 }
--- test/FallbackRouter.t.sol
+++ test/FallbackRouter.t.sol
@@ -2,60 +2,43 @@
 
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import { BaseTest } from "test/base/BaseTest.t.sol";
+import { ADMIN_ROLE, MEMBER_ROLE } from "src/dao_access/Roles.sol";
 
-import {ADMIN_ROLE} from "src/dao_access/Roles.sol";
-import {DaoAccess} from "src/dao_access/DaoAccess.sol";
-import {FallbackRouter} from "src/fallback_router/FallbackRouter.sol";
+import { FallbackRouter } from "src/fallback_router/FallbackRouter.sol";
 
-contract FallbackRouter_test is Test {
-    FallbackRouter internal router;
-    address internal ROUTER;
-
-    address internal constant OWNER = address(501);
-    address internal constant USER = address(1);
+contract FallbackRouter_test is BaseTest {
+    FallbackRouter internal router_impl;
+    FallbackRouter internal dao;
 
     function setUp() public {
-        // write OWNER's admin role in Router storage
-        DaoAccess access = new DaoAccess(OWNER);
-        vm.prank(OWNER);
-        access.bootstrap();
-
-        // deploy a fallback router
-        FallbackRouter r = new FallbackRouter();
-
-        // assign router code to access
-        vm.etch(address(access), address(r).code);
-        router = FallbackRouter(address(access));
-        ROUTER = address(router);
+        _newUsersSet(0, 5);
+        (router_impl, ) = _deployDAO();
+        dao = FallbackRouter(DAO);
     }
 
     function test_updateFunction_UpdateAFunction() public {
         bytes4 selector = 0xbad00000;
         address impl = address(0xbad00000);
         vm.prank(OWNER);
-        router.updateFunction(selector, impl);
+        dao.updateFunction(selector, impl);
 
-        assertEq(router.getFunctionImpl(selector), impl);
+        assertEq(dao.getFunctionImpl(selector), impl);
     }
 
     function test_updateFunction_OnlyOwnerCanUpdate() public {
         bytes4 selector = 0xbad00000;
         address impl = address(0xbad00000);
 
-        vm.prank(USER);
+        vm.prank(USERS[0]);
 
         vm.expectRevert(
-            abi.encodeWithSignature(
-                "MissingRole(address,bytes32)",
-                USER,
-                ADMIN_ROLE
-            )
+            abi.encodeWithSignature("MissingRole(address,bytes32)", USERS[0], ADMIN_ROLE)
         );
-        router.updateFunction(selector, impl);
+        dao.updateFunction(selector, impl);
     }
 
     function test_getSelectorList_ReturnListOfSelectors() public {
-        router.getSelectorList();
+        dao.getSelectorList();
     }
 }
